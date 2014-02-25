be3608d8cbabb15478027ea6c33fe98e32d8f600
Ulysse Goarant <ulysses.goarant@gmail.com>
2014-02-25 18:22:03 +0100

    Ajout rapport TP4

--- TP4/ArffCreator.pl
+++ TP4/ArffCreator.pl
@@ -11,34 +11,24 @@ my @words = ();
 open my $words, $wordsfile or die "Could not open $file: $!";
 open (my $arff, '>articles.10p.arff');
 
+print $arff "\@relation articles.symbolic\n\n";
+
 while( my $line = <$words>)  {   
     print $line;
     chomp $line;
     push(@words, $line);
 
     print $arff "\@attribute $line {present, absent}\n"
-
-    #print $arff "Salut\n";
-    #last if $. == 2;
 }
 
 print $arff "\n\@data\n";
 
-
-
-print "\n\n\n@words\n";
-
-#exit 0;
-
 open my $info, $file or die "Could not open $file: $!";
 
-
 while( my $line = <$info>)  {   
-    #print $line;
 
     my $i = 0;
     foreach(@words) {
-    	#print $_;
     	if($line =~ /\s*$_\s*/) {
     		print $arff "present";
     	} else {
@@ -53,10 +43,8 @@ while( my $line = <$info>)  {
     }
 
     print $arff "\n";
-
-    #print $arff "Salut\n";
-    #last if $. == 2;
 }
 
 close $arff;
+close $wordsfile;
 close $info;
