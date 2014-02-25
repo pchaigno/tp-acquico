e3b3a28b77d7c1d3abd1d5f7675ac418921174a6
Ulysse Goarant <ulysses.goarant@gmail.com>
2014-02-25 17:51:24 +0100

    correction script arff

--- TP4/ArffCreator.pl
+++ TP4/ArffCreator.pl
@@ -9,15 +9,20 @@ my $wordsfile = 'mots.lst';
 my @words = ();
 
 open my $words, $wordsfile or die "Could not open $file: $!";
+open (my $arff, '>articles.10p.arff');
 
 while( my $line = <$words>)  {   
     print $line;
     chomp $line;
     push(@words, $line);
 
+    print $arff "\@attribute $line {present, absent}\n"
+
     #print $arff "Salut\n";
     #last if $. == 2;
 }
+
+print $arff "\n\@data\n";
 
 
 
@@ -26,22 +31,28 @@ print "\n\n\n@words\n";
 #exit 0;
 
 open my $info, $file or die "Could not open $file: $!";
-open (my $arff, '>>articles.10p.arff');
+
 
 while( my $line = <$info>)  {   
     #print $line;
 
+    my $i = 0;
     foreach(@words) {
     	#print $_;
     	if($line =~ /\s*$_\s*/) {
-    		print $arff "$_, ";
+    		print $arff "present";
     	} else {
-    		print $arff "pas" . "$_, ";
+    		print $arff "absent";
     	}
+
+    	if ($i < $#words) {
+    		print $arff ",";
+    	}
+
+    	$i++;
     }
 
-    print "\n";
-    last;
+    print $arff "\n";
 
     #print $arff "Salut\n";
     #last if $. == 2;
